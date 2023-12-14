4247828344b0108e95ed8cd41a5285279380a1eb
gui-araujo-21 <guiaraujorodrigues21@gmail.com>
2023-12-14 12:04:20 -0300

    added all calculator operations

--- script.js
+++ script.js
@@ -35,9 +35,72 @@ const sum = ()=>{
 }   
 
 const minus = ()=>{
-    
+    if(number1 == 0){
+        number1 = parseFloat(numberDisplay);
+        numberDisplay = '';
+    }else{
+        number2 = parseFloat(numberDisplay);
+        resultOper = number1 - number2;
+        result();
+    }
     oper = 'minus';
-}   
+}
+
+const mult = ()=>{
+    if(number1 == 0){
+        number1 = parseFloat(numberDisplay);
+        numberDisplay = '';
+    }else{
+        number2 = parseFloat(numberDisplay);
+        resultOper = number1 * number2;
+        result();
+    }
+    oper = 'mult';
+}
+
+const division = ()=>{
+    if(number1 == 0){
+        number1 = parseFloat(numberDisplay);
+        numberDisplay = '';
+    }else{
+        number2 = parseFloat(numberDisplay);
+        resultOper = number1 / number2;
+        result();
+    }
+    
+    oper = 'division';
+}
+
+const percent = ()=>{
+    if(numberDisplay == ''){
+        numberDisplay = 0;
+    }else{
+        numberDisplay = parseFloat(numberDisplay);
+    }
+    numberDisplay /= 100;
+    renderDisplay();
+}
+
+const inverseSign = ()=>{
+    if(numberDisplay == ''){
+        numberDisplay = 0;
+    }else{
+        numberDisplay = parseFloat(numberDisplay);
+    }
+    numberDisplay *= (-1);
+    renderDisplay();
+}
+
+const addDot = ()=>{
+    if(numberDisplay == ''){
+        numberDisplay = '0';
+    }
+    
+    numberDisplay += '.';
+    renderDisplay();
+}
+
+
 
 
 const result = ()=>{
@@ -45,11 +108,13 @@ const result = ()=>{
     switch(oper){
         case 'sum':
             if(number1 == 0){
+                console.log('AAAAA')
                 resultOper += number2;
                 numberDisplay = resultOper.toString();
                 renderDisplay();
             }
             else if(number2 == 0){
+                console.log('BBBB')
                 number2 = parseFloat(numberDisplay);
                 resultOper = number1 + number2;
                 numberDisplay = resultOper.toString();
@@ -57,6 +122,7 @@ const result = ()=>{
                 number1 = 0;
         
             }else{
+                console.log('CCCCC')
                 number2 = parseFloat(numberDisplay);
                 resultOper = number1 + number2;
                 numberDisplay = resultOper.toString();
@@ -64,9 +130,70 @@ const result = ()=>{
                 number1 = 0;
             }
             break;
-       
-            
 
+        case 'minus':
+            if(number1 == 0){
+                resultOper -= number2;
+                numberDisplay = resultOper.toString();
+                renderDisplay();
+            }
+            else if(number2 == 0){
+                number2 = parseFloat(numberDisplay);
+                resultOper = number1 - number2;
+                numberDisplay = resultOper.toString();
+                renderDisplay();
+                number1 = 0;
+        
+            }else{
+                number2 = parseFloat(numberDisplay);
+                resultOper = number1 - number2;
+                numberDisplay = resultOper.toString();
+                renderDisplay();
+                number1 = 0;
+            }
+            break;
+        case 'mult':
+            if(number1 == 0){
+                resultOper *= number2;
+                numberDisplay = resultOper.toString();
+                renderDisplay();
+            }
+            else if(number2 == 0){
+                number2 = parseFloat(numberDisplay);
+                resultOper = number1 * number2;
+                numberDisplay = resultOper.toString();
+                renderDisplay();
+                number1 = 0;
+        
+            }else{
+                number2 = parseFloat(numberDisplay);
+                resultOper = number1 * number2;
+                numberDisplay = resultOper.toString();
+                renderDisplay();
+                number1 = 0;
+            }
+            break;
+        case 'division':
+            if(number1 == 0){
+                resultOper /= number2;
+                numberDisplay = resultOper.toString();
+                renderDisplay();
+            }
+            else if(number2 == 0){
+                number2 = parseFloat(numberDisplay);
+                resultOper = number1 / number2;
+                numberDisplay = resultOper.toString();
+                renderDisplay();
+                number1 = 0;
+        
+            }else{
+                number2 = parseFloat(numberDisplay);
+                resultOper = number1 / number2;
+                numberDisplay = resultOper.toString();
+                renderDisplay();
+                number1 = 0;
+            }
+            break;
     }
 
 }
@@ -97,6 +224,16 @@ qSA('.number').forEach((item)=>{
 qS('.sum').addEventListener('click', sum);
 
 qS('.minus').addEventListener('click', minus);
+
+qS('.mult').addEventListener('click', mult);
+
+qS('.division').addEventListener('click', division);
+
+qS('.percent').addEventListener('click',percent);
+
+qS('.inverseSign').addEventListener('click',inverseSign);
+
+qS('.dot').addEventListener('click',addDot);
 
 qS('.result').addEventListener('click', result);
 
